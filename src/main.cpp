0a687d41a6f9ab7812d608a657bce8722fb332ba
Rob Burrus <robertburrus24@gmail.com>
2017-05-30 20:46:28 -0400

    First Implementation

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,12 @@ constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
+
+const int num_params = 3;
+double dp[num_params] = {1.0, 1.0, 1.0};
+double tol = 0.001;
+double speed_target = 50.0;
+
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
@@ -32,10 +38,15 @@ int main()
 {
   uWS::Hub h;
 
-  PID pid;
-  // TODO: Initialize the pid variable.
+  PID pid_steer;
+  PID pid_speed;
+  
+  pid_steer.Init(0.1, 0.001, 2.5);
+  pid_speed.Init(0.1, 0.001, 2.5);
+  
+  
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+  h.onMessage([&pid_speed, &pid_steer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -50,20 +61,44 @@ int main()
           double cte = std::stod(j[1]["cte"].get<std::string>());
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          double steer_value;
-          /*
-          * TODO: Calcuate steering value here, remember the steering value is
-          * [-1, 1].
-          * NOTE: Feel free to play around with the throttle and speed. Maybe use
-          * another PID controller to control the speed!
-          */
+          
+          //Steering Value
+          pid_steer.UpdateError(cte);
+          double steer_value = pid_steer.TotalError();
+          if(steer_value > 1.0){
+                steer_value = 1.0;
+          }else if( steer_value < -1.0){
+                steer_value = -1.0;
+          }
+          
+          //Speed Value
+          
+          double speed_error = (speed - speed_target) / speed_target;
+          pid_speed.UpdateError(speed_error);
+          
+          double speed_value = pid_speed.TotalError();
+          if(speed_value > 1.0){
+            speed_value = 1.0;
+          }else if( speed_value < -1.0){
+            speed_value = -1.0;
+          }
+          
+          // apply brake if sharp steering angle, and driving over 40mph
+          if (fabs(steer_value) > 0.4 && speed > 40.0) {
+            speed_value = -1.0;
+          }
+          
           
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << " Speed Value: " << speed_value << std::endl;
+          
+          
+          
+          
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = speed_value;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
